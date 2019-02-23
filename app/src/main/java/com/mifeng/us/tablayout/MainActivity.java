3afd50d23810cdfb8b7021319b57c667f87af2ec
wdx <flyingtercel@163.com>
2019-02-23 16:02:10 +0800

    v1.0.0

--- app/src/main/java/com/mifeng/us/tablayout/MainActivity.java
+++ app/src/main/java/com/mifeng/us/tablayout/MainActivity.java
@@ -26,41 +26,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mHbElasticView = (HbElasticView) findViewById(R.id.hb);
         myIndicator = (MyIndicator) findViewById(R.id.my);
-        findViewById(R.id.btn_add).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myIndicator.add("tab5", 10);
-                myIndicator.setChanger(4);
-                Toast.makeText(MainActivity.this, "添加tab5", Toast.LENGTH_SHORT).show();
-            }
+        findViewById(R.id.btn_add).setOnClickListener(v->{
+            myIndicator.add("tab5", 10);
+            myIndicator.setChanger(4);
+            Toast.makeText(MainActivity.this, "添加tab5", Toast.LENGTH_SHORT).show();
         });
-        findViewById(R.id.btn_speed).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myIndicator.setAnimationTime(100);
-                Toast.makeText(MainActivity.this, "修改移动速度改为100", Toast.LENGTH_SHORT).show();
-            }
+        findViewById(R.id.btn_speed).setOnClickListener(v -> {
+            myIndicator.setAnimationTime(100);
+            Toast.makeText(MainActivity.this, "修改移动速度改为100", Toast.LENGTH_SHORT).show();
         });
-        findViewById(R.id.btn_jinzhi).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myIndicator.setProhibitPositio(2);
-                Toast.makeText(MainActivity.this, "禁止tab3修改状态", Toast.LENGTH_SHORT).show();
-            }
+        findViewById(R.id.btn_jinzhi).setOnClickListener(v -> {
+            myIndicator.setProhibitPositio(2);
+            Toast.makeText(MainActivity.this, "禁止tab3修改状态", Toast.LENGTH_SHORT).show();
         });
-        findViewById(R.id.btn_length).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myIndicator.setFull();
-                Toast.makeText(MainActivity.this, "修改下标线的长度铺满", Toast.LENGTH_SHORT).show();
-            }
+        findViewById(R.id.btn_length).setOnClickListener(v -> {
+            myIndicator.setFull();
+            Toast.makeText(MainActivity.this, "修改下标线的长度铺满", Toast.LENGTH_SHORT).show();
         });
-        findViewById(R.id.btn_color).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myIndicator.resetNomalColor(0xff000000,0xffffffff,0xff000000);
-            }
+        findViewById(R.id.btn_color).setOnClickListener(v -> {
+            myIndicator.resetNomalColor(0xff000000,0xffffffff,0xff000000);
         });
+
     }
 
 
